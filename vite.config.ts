8e8549385c9c6106c3db0abb4951fba87c4b348b
petya <petyamaiko@gmail.com>
2025-02-26 16:52:29 +0200

    work

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,41 @@
-import { defineConfig } from 'vite'
+import type { ResolvedConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { checker } from 'vite-plugin-checker'
 
 // https://vite.dev/config/
-export default defineConfig({
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler', // or "modern"
-      },
-    },
-  },
-  server: {
-    port: 8881,
-  },
-  plugins: [
-    vue(),
-    checker({
-      vueTsc: {
-        tsconfigPath: 'tsconfig.app.json',
-      },
-    }),
-  ],
+export default ({ mode }: ResolvedConfig) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
-  resolve: {
-    alias: {
-      '~': '/src',
+  return defineConfig({
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
     },
-  },
-})
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler', // or "modern"
+        },
+      },
+    },
+    server: {
+      port: Number(process.env.VITE_SERVER_PORT) || 3000,
+    },
+    plugins: [
+      vue(),
+      checker({
+        vueTsc: {
+          tsconfigPath: 'tsconfig.app.json',
+        },
+      }),
+    ],
+
+    resolve: {
+      alias: {
+        '~': '/src',
+      },
+    },
+  })
+}
+
